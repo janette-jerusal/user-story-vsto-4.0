c935ee9f610330406bd408b4626028fcdb03f56a
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-10 11:41:16 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,23 +1,33 @@
+using System;
 using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Core;
 
-namespace UserStorySimilarityAddInFixed
+namespace UserStorySimilarityAddIn
 {
     public partial class ThisAddIn
     {
+        private MyRibbon ribbon;
+
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+            // Nothing required here
+        }
+
+        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        {
+            // Clean-up code here
+        }
+
         protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
             return new MyRibbon();
         }
 
-        private void ThisAddIn_Startup(object sender, System.EventArgs e) { }
-
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e) { }
-
         private void InternalStartup()
         {
-            this.Startup += ThisAddIn_Startup;
-            this.Shutdown += ThisAddIn_Shutdown;
+            this.Startup += new EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new EventHandler(ThisAddIn_Shutdown);
         }
     }
 }
