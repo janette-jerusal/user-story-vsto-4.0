848f6b58446e230ef5ddf7a21a0ad984c0cc678a
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-10 11:40:51 -0400

    Update MyRibbon.cs

--- MyRibbon.cs
+++ MyRibbon.cs
@@ -1,9 +1,9 @@
-using System.IO;
 using System.Reflection;
+using System.IO;
 using System.Windows.Forms;
 using Microsoft.Office.Core;
 
-namespace UserStorySimilarityAddInFixed
+namespace UserStorySimilarityAddIn
 {
     public class MyRibbon : IRibbonExtensibility
     {
@@ -11,31 +11,33 @@ namespace UserStorySimilarityAddInFixed
 
         public MyRibbon()
         {
+            // Confirm constructor runs at startup
             MessageBox.Show("Ribbon Constructor Called");
         }
 
         public string GetCustomUI(string ribbonID)
         {
             MessageBox.Show("GetCustomUI triggered!");
+
             var assembly = Assembly.GetExecutingAssembly();
             var resources = assembly.GetManifestResourceNames();
-            MessageBox.Show("Resources:\n" + string.Join("\n", resources));
+            MessageBox.Show("Embedded Resources:\n" + string.Join("\n", resources));
 
-            foreach (string res in resources)
+            foreach (var res in resources)
             {
-                if (res.EndsWith("CustomRibbon.xml"))
+                if (res.EndsWith("MvRibbon.xml", System.StringComparison.OrdinalIgnoreCase))
                 {
                     using (Stream stream = assembly.GetManifestResourceStream(res))
                     using (StreamReader reader = new StreamReader(stream))
                     {
-                        var xml = reader.ReadToEnd();
-                        MessageBox.Show("Ribbon XML content:\n" + xml);
+                        string xml = reader.ReadToEnd();
+                        MessageBox.Show("Ribbon XML loaded:\n" + xml.Substring(0, Math.Min(xml.Length, 200)));
                         return xml;
                     }
                 }
             }
 
-            MessageBox.Show("CustomRibbon.xml not found in resources.");
+            MessageBox.Show("MvRibbon.xml not found!");
             return null;
         }
 
